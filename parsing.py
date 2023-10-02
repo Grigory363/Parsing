dfc88ff7a63c89561b109732c4fe8e8717f996c7
Григорий Михалев <mihalev363@gmail.com>
2023-10-02 21:09:43 +0300

    parsingcom
    commit

---
+++ parsing.py
@@ -0,0 +1,29 @@
+import random
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import json
+import lxml
+
+url = "https://www.globus.ru/catalog/chay-kofe-kakao/kofe/"
+headers = {
+    "Accept": "*/*",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15"
+}
+
+req = requests.get(url, headers=headers)
+src = req.text
+# print(src)
+
+# with open ("index.html", "w") as file:
+#     file.write(src)
+
+with open ("index.html") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
+cofee_href = soup.find_all(class_="pim-list__item pim-list__item--standart ga-event")
+for item in cofee_href:
+    item_text = item.text.lstrip()
+    item_href = "https://www.globus.ru" + item.get("href") + item_text
+    print(item_href)
